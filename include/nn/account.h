76e8b9426a00155a8dae7a9689c2921b128d0372
fruityloops1 <fruityloops@littun.co>
2023-11-21 14:20:27 +0100

    LoadProfileImage

--- include/nn/account.h
+++ include/nn/account.h
@@ -10,48 +10,49 @@
 
 namespace nn {
 namespace account {
-typedef char Nickname[0x21];
-typedef u64 NetworkServiceAccountId;
+    typedef char Nickname[0x21];
+    typedef u64 NetworkServiceAccountId;
 
-class AsyncContext;
+    class AsyncContext;
 
-class Uid {
-public:
-    bool IsValid() const { return m_Storage[0] != 0 || m_Storage[1] != 0; }
+    class Uid {
+    public:
+        bool IsValid() const { return m_Storage[0] != 0 || m_Storage[1] != 0; }
 
-    u64 m_Storage[2];
-};
+        u64 m_Storage[2];
+    };
 
-class UserHandle {
-public:
-    Uid m_Uid;
-    void* m_Handle;
-};
+    class UserHandle {
+    public:
+        Uid m_Uid;
+        void* m_Handle;
+    };
 
-void Initialize();
-Result ListAllUsers(s32*, Uid*, s32 numUsers);
-Result OpenUser(UserHandle*, Uid const&);
-Result IsNetworkServiceAccountAvailable(bool* out, UserHandle const&);
-void CloseUser(UserHandle const&);
+    void Initialize();
+    Result ListAllUsers(s32*, Uid*, s32 numUsers);
+    Result OpenUser(UserHandle*, Uid const&);
+    Result IsNetworkServiceAccountAvailable(bool* out, UserHandle const&);
+    void CloseUser(UserHandle const&);
 
-Result EnsureNetworkServiceAccountAvailable(UserHandle const& userHandle);
-Result EnsureNetworkServiceAccountIdTokenCacheAsync(AsyncContext*, UserHandle const&);
-Result LoadNetworkServiceAccountIdTokenCache(u64*, char*, u64, UserHandle const&);
+    Result EnsureNetworkServiceAccountAvailable(UserHandle const& userHandle);
+    Result EnsureNetworkServiceAccountIdTokenCacheAsync(AsyncContext*, UserHandle const&);
+    Result LoadNetworkServiceAccountIdTokenCache(u64*, char*, u64, UserHandle const&);
 
-Result GetLastOpenedUser(Uid*);
-Result GetNickname(Nickname* nickname, Uid const& userID);
+    Result GetLastOpenedUser(Uid*);
+    Result GetNickname(Nickname* nickname, Uid const& userID);
+    Result LoadProfileImage(size_t*, void*, size_t, const Uid&);
 
-Result GetUserId(Uid* uid, const UserHandle& handle);
-Result OpenPreselectedUser(UserHandle* handle);
+    Result GetUserId(Uid* uid, const UserHandle& handle);
+    Result OpenPreselectedUser(UserHandle* handle);
 
-class AsyncContext {
-public:
-    AsyncContext();
+    class AsyncContext {
+    public:
+        AsyncContext();
 
-    Result HasDone(bool*);
-    Result GetResult();
-    Result Cancel();
-    Result GetSystemEvent(nn::os::SystemEvent*);
-};
-}  // namespace account
-}  // namespace nn
+        Result HasDone(bool*);
+        Result GetResult();
+        Result Cancel();
+        Result GetSystemEvent(nn::os::SystemEvent*);
+    };
+} // namespace account
+} // namespace nn
